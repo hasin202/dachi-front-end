270f6b0628f21d31a5f7bfbab2aae078a33fdd69
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-04-26 16:39:10 +0100

    working nested links using react router

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,15 +1,26 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 const Nav = () => {
+  const linkNames = ["Marketplace", "Cart", "My Tickets", "Card Details"];
+  const linkPaths = ["/marketplace", "/cart", "/my-tickets", "/card-details"];
+
   return (
-    <div className="w-1/5 bg-slate-400 text-3xl font-bold">
-      <p>DACHI</p>
-      <nav className="flex flex-col">
-        <Link to="/marketplace" className="">
-          Marketplace
-        </Link>
-        <Link to="/cart" className="">
-          Cart
-        </Link>
+    <div className="w-1/4 pr-4 py-8 border-r-2 border-gray-400">
+      <p className="text-3xl font-black mb-4">DACHI</p>
+      <nav className="flex flex-col gap-1 text-xl">
+        {linkNames.map((link, i) => (
+          <NavLink
+            to={linkPaths[i]}
+            className={({ isActive }) =>
+              isActive
+                ? "bg-black px-2 py-1 text-white rounded font-light"
+                : "px-2 py-1 font-light"
+            }
+            key={i}
+          >
+            {link}
+          </NavLink>
+        ))}
       </nav>
     </div>
   );
