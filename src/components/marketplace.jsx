0373e9ec78b815fa888aaae7a424dfc1e7470978
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-04-27 21:37:11 +0100

    moved getting table data

--- src/components/marketplace.jsx
+++ src/components/marketplace.jsx
@@ -1,8 +1,22 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-
+import Ticket from "./ticket.jsx";
 const Marketplace = () => {
-  return <p className="w-full bg-red-700">marketplace</p>;
+  const [fetchedData, setFetchedData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get("http://localhost:3000/tickets");
+      setFetchedData(data.data);
+    };
+    getData();
+  }, []);
+  return (
+    <div className="flex flex-col w-full px-4 gap-8 h-full overflow-y-scroll">
+      {fetchedData.map((data) => (
+        <Ticket key={data.ticket_id} {...data} />
+      ))}
+    </div>
+  );
 };
 
 export default Marketplace;
