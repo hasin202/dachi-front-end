45f7046841f439f55f5c44689e67653030e44d3d
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-04-28 17:59:17 +0100

    imported component for individual tickets

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from "./components/nav.jsx";
 import Marketplace from "./components/marketplace.jsx";
 import Cart from "./components/cart.jsx";
 import UserTickets from "./components/users_tickets.jsx";
+import IndividualTicket from "./components/individual_ticket.jsx";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Nav />
         <Routes>
           <Route path="marketplace" element={<Marketplace />} />
+          <Route path="marketplace/:ticket_id" element={<IndividualTicket />} />
           <Route path="cart" element={<Cart />} />
           <Route path="my-tickets" element={<UserTickets />} />
         </Routes>
