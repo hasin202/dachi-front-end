15b9945c14af89ebc96108dfbc919648bb5771dc
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-04-28 20:01:45 +0100

    passed ticket data as props using useLocation hook Also imported error component for display errors

--- src/components/individual_ticket.jsx
+++ src/components/individual_ticket.jsx
@@ -1,7 +1,14 @@
+import { useLocation } from "react-router-dom";
+import Error from "./error";
+
 const IndividualTicket = () => {
-    console.log("click!")
-    return <p className="w-full bg-red-700">Individual Ticket</p>;
-  };
-  
-  export default IndividualTicket;
-  
+  const { state } = useLocation();
+  if (!state) {
+    return <Error />;
+  }
+  const { ticket_id, event_name, address, postcode, price, start, end } =
+    state.data || {};
+  return <div className="w-full">{event_name}</div>;
+};
+
+export default IndividualTicket;
