a11502c3c3baf0abc8d7bddfbd69c493d3559464
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-04-29 14:30:01 +0100

    recieve object for error info as a prop

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -1,14 +1,15 @@
-import error from "../assets/404.png";
+import errorImg from "../assets/404.png";
 
-const Error = () => {
+const Error = ({ error }) => {
+  const { heading, body } = error;
   return (
     <div className="w-full flex flex-col gap-2">
-      <img src={error} />
-      <p className="text-5xl w-full text-center font-bold">
-        NOT A VALID TICKET!
+      <img src={errorImg} />
+      <p className="text-5xl w-full text-center font-bold uppercase">
+        {heading}
       </p>
       <p className="text-2xl w-full text-center font-light text-gray-500">
-        Please select one from the list of displayed tickets
+        {body}
       </p>
     </div>
   );
