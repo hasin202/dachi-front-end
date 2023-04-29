fb85f8e7172a1986aa84cea201c9d96e459e0ccd
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-04-29 14:44:45 +0100

    made 404 image smaller

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -3,8 +3,8 @@ import errorImg from "../assets/404.png";
 const Error = ({ errorInfo }) => {
   const { heading, body } = errorInfo;
   return (
-    <div className="w-full flex flex-col gap-2">
-      <img src={errorImg} />
+    <div className="w-full flex flex-col gap-2 items-center justify-center">
+      <img className="w-96 h-96" src={errorImg} />
       <p className="text-5xl w-full text-center font-bold uppercase">
         {heading}
       </p>
