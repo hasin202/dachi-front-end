dbb96272ecdc5a62d5dee48d0504735389c3967b
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-04-30 13:38:30 +0100

    only display cart if cart isn't empty

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,12 +1,14 @@
 const Cart = () => {
-  console.log(JSON.parse(localStorage.getItem("cart")));
   const cart = JSON.parse(localStorage.getItem("cart"));
+
   return (
-    <div className="w-full bg-red-700">
-      {cart.map((ticket) => (
-        <p key={ticket.ticket_id}>{ticket.ticket_id}</p>
-      ))}
-    </div>
+    cart && (
+      <div className="w-full bg-red-700">
+        {cart.map((ticket) => (
+          <p key={ticket.ticket_id}>{ticket.ticket_id}</p>
+        ))}
+      </div>
+    )
   );
 };
 
