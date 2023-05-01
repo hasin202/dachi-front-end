302e1645269ab2a9278f5e2fcab06e6dc8981228
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-05-01 18:45:52 +0100

    add button to clear cart and buy tickets

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -3,7 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import Ticket from "./tickets";
 
 const Cart = () => {
-  const { cart } = useContext(cartContext);
+  const { cart, clearCart } = useContext(cartContext);
   const [total, setTotal] = useState(0);
 
   // const cart = JSON.parse(localStorage.getItem("cart"));
@@ -13,13 +13,6 @@ const Cart = () => {
     }, 0);
     setTotal(finalPrice);
   }, [cart]);
-  // console.log(total);
-
-  // let total = cart.reduce(function (acc, obj) {
-  //   return acc + obj.price;
-  // }, 0);
-
-  // console.log(total);
 
   return (
     cart && (
@@ -29,14 +22,25 @@ const Cart = () => {
             <Ticket key={ticket.ticket_id} data={ticket} />
           ))}
         </div>
-        <div className="flex flex-col w-full border-t mx-4 border-gray-400 py-2 pr-8">
+        <div className="flex flex-col w-full border-t mx-4 border-gray-400 pt-2 pr-8 gap-2">
           <div className="flex w-full justify-between">
             <p className="text-xl font-bold">TOTAL</p>
             <p className="text-xl font-bold font-light">{`£${total}`}</p>
           </div>
-          <button className="border border-purple-700 text-purple-700 w-full rounded py-1 font-light">
-            Clear cart
-          </button>
+          <div className="flex gap-2">
+            <button
+              className="border border-purple-700 text-purple-700 w-full rounded py-1 font-light"
+              onClick={clearCart}
+            >
+              Clear cart
+            </button>
+            <button
+              className="bg-purple-700 text-white w-full rounded py-1 font-light disabled:bg-black"
+              disabled={!total}
+            >
+              Buy tickets
+            </button>
+          </div>
         </div>
       </div>
     )
