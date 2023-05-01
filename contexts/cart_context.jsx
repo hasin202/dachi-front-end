237c0a35d933ca6be856fc0edf0b0e6fe3b558f9
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-05-01 15:43:03 +0100

    create cart context

--- contexts/cart_context.jsx
+++ contexts/cart_context.jsx
@@ -1,16 +1,5 @@
 import { createContext } from "react";
 
-export const cartContext = createContext();
+const cartContext = createContext();
 
-const sayHi = () => {
-  console.log("hi");
-};
-
-const CartContextProvider = ({ children }) => {
-  return (
-    <cartContext.Provider value={{ name: "hasin", age: 20, sayHi: sayHi }}>
-      {children}
-    </cartContext.Provider>
-  );
-};
-export default CartContextProvider;
+export default cartContext;
