6e1aa5a79d3e9fa3284cb1276892ce9164a7b36e
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-05-01 15:45:38 +0100

    cart component for showing tickets in cart

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,12 +1,20 @@
+import cartContext from "../../contexts/cart_context";
+import { useContext } from "react";
+import Ticket from "./tickets";
+
 const Cart = () => {
-  const cart = JSON.parse(localStorage.getItem("cart"));
+  const { cart } = useContext(cartContext);
+  // const cart = JSON.parse(localStorage.getItem("cart"));
 
   return (
     cart && (
-      <div className="w-full bg-red-700">
-        {cart.map((ticket) => (
-          <p key={ticket.ticket_id}>{ticket.ticket_id}</p>
-        ))}
+      <div className="w-full px-4 h-full">
+        <div className="flex flex-col w-full px-4 gap-8 h-5/6 overflow-y-scroll">
+          {cart.map((ticket) => (
+            <Ticket key={ticket.ticket_id} data={ticket} />
+          ))}
+        </div>
+        <div className="border-t border-gray-400">Total</div>
       </div>
     )
   );
