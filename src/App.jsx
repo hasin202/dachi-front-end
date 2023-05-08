05d549384751f77fb20930d9880e8e544acb7428
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-05-08 23:09:46 +0100

    developing auth front end

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,28 @@ import UserTickets from "./components/users_tickets.jsx";
 import IndividualTicket from "./components/individual_ticket.jsx";
 import Reducer from "../contexts/useReduce.jsx";
 
+// export default function App() {
+//   return (
+//     <>
+//       <div className="border-box flex px-32 py-24 h-screen">
+//         <Nav />
+//         <Routes>
+//           <Route path="/" element={<Reducer />} />
+//           <Route path="marketplace" element={<Marketplace />} />
+//           <Route path="marketplace/:ticket_id" element={<IndividualTicket />} />
+//           <Route path="cart/:ticket_id" element={<IndividualTicket />} />
+//           <Route path="cart" element={<Cart />} />
+//           <Route path="my-tickets" element={<UserTickets />} />
+//         </Routes>
+//       </div>
+//     </>
+//   );
+// }
+
 export default function App() {
   return (
     <>
-      <div className="border-box flex px-32 py-24 h-screen">
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Reducer />} />
-          <Route path="marketplace" element={<Marketplace />} />
-          <Route path="marketplace/:ticket_id" element={<IndividualTicket />} />
-          <Route path="cart/:ticket_id" element={<IndividualTicket />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="my-tickets" element={<UserTickets />} />
-        </Routes>
-      </div>
+      <p>hello</p>
     </>
   );
 }
