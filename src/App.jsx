acf1b21b3e134a74be05d364128bc59cb7ff2e06
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-05-08 22:49:52 +0100

    removed unnesccessary http call

--- src/App.jsx
+++ src/App.jsx
@@ -11,67 +11,42 @@ import Home from "./components/home.jsx";
 import SignUp from "./components/auth/signup.jsx";
 import LogIn from "./components/auth/login.jsx";
 import axios from "axios";
-import Testcom from "./components/authorised_home.jsx";
+import TicketingHome from "./components/authorised_home.jsx";
+
+function ProtectedRoute({ element, ...rest }) {
+  console.log(`Token: ${localStorage.getItem("token")}`);
+  const auth = localStorage.getItem("token");
+  return (
+    <div className="w-full">{auth ? element : <Navigate to="/SignIn" />}</div>
+  );
+}
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    // Check if the user is authenticated by sending a request to the server
-    const token = localStorage.getItem("token");
-    if (token) {
-      axios
-        .get("http://localhost:8080/api/test", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        })
-        .then((res) => {
-          setIsAuthenticated(true);
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    }
-  }, []);
-
-  const handleSignOut = () => {
-    localStorage.removeItem("token");
-    setIsAuthenticated(false);
-    console.log("Logged out");
-  };
+  // useEffect(() => {
+  //   console.log(`token ${localStorage.getItem("token")}`);
+  // }, []);
 
   return (
     <AuthProvider>
-      <div className="border-box flex px-32 py-24 h-screen">
-        {/* <Nav /> */}
+      <div className="border-box w-full h-screen flex px-32 py-24 h-screen">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="signup" element={<SignUp />} />
           <Route path="login" element={<LogIn />} />
-          <Route path="marketplace" element={<Marketplace />} />
-          <Route path="marketplace/:ticket_id" element={<IndividualTicket />} />
+          {/* <Route path="marketplace" element={<Marketplace />} /> */}
+          {/* <Route path="marketplace/:ticket_id" element={<IndividualTicket />} />
           <Route path="cart/:ticket_id" element={<IndividualTicket />} />
           <Route path="cart" element={<Cart />} />
-          <Route path="my-tickets" element={<UserTickets />} />
+          <Route path="my-tickets" element={<UserTickets />} /> */}
           <Route
             path="/Testcom/*"
-            element={
-              <ProtectedRoute
-                isAuthenticated={isAuthenticated}
-                element={<Testcom handleSignOut={handleSignOut} />}
-              />
-            }
+            element={<ProtectedRoute element={<TicketingHome />} />}
           />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </AuthProvider>
   );
-}
-
-function ProtectedRoute({ isAuthenticated, element, ...rest }) {
-  return isAuthenticated ? element : <Navigate to="/SignIn" />;
 }
 
 export default App;
