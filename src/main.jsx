fdcec1f8eeedb65a598e495e773b8df783819a54
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-05-09 03:03:08 +0100

    update imports

--- src/main.jsx
+++ src/main.jsx
@@ -26,8 +26,7 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "../contexts/auth/AuthProvider";
-import CartContextProvider from "../contexts/cart_context_provider.jsx";
-
+import CartContextProvider from "../contexts/cart/cart_context_provider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
