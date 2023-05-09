dce7258dce58d5cd1004bb07b595f9003de0ee68
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-05-09 02:17:55 +0100

    moved into auth folder

--- contexts/auth/AuthProvider.jsx
+++ contexts/auth/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { supabase } from "../src/supabase/client";
+import { supabase } from "../../src/supabase/client";
 
 const AuthContext = createContext({});
 
