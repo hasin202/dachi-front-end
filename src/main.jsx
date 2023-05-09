918af5a5adfc7e39444c40268494d4665cccdd8a
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-05-09 01:23:59 +0100

    wrap all components in auth context

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,41 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { BrowserRouter } from "react-router-dom";
+// import CartContextProvider from "../contexts/cart_context_provider.jsx";
+// import AuthProvider from "../contexts/auth/AuthProvider";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <AuthProvider>
+//         <CartContextProvider>
+//           <App />
+//         </CartContextProvider>
+//       </AuthProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "../contexts/AuthProvider";
 import CartContextProvider from "../contexts/cart_context_provider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CartContextProvider>
-        <App />
-      </CartContextProvider>
+      <AuthProvider>
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
