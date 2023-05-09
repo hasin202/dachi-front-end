306f15a8fd7edb8c7e58ab872cf22fbc0b24809a
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-05-09 05:20:41 +0100

    display user tickets with Ticket component

--- src/components/ticket_components/users_tickets.jsx
+++ src/components/ticket_components/users_tickets.jsx
@@ -2,18 +2,21 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Error from "../error";
 import { useAuth } from "../../../contexts/auth/AuthProvider";
+import Ticket from "./tickets";
+import { NavLink } from "react-router-dom";
 
 const UserTickets = () => {
   const { user } = useAuth();
+  console.log(user.id);
   const [error, setError] = useState();
-
   const [fetchedData, setFetchedData] = useState([]);
+
   useEffect(() => {
     const getData = async () => {
       try {
         const res = await axios.get(`http://localhost:3012/user/${user.id}`);
         const data = res.data;
-        setFetchedData(data[0]);
+        setFetchedData(data);
       } catch (error) {
         setError(error.response.data);
       }
@@ -22,16 +25,22 @@ const UserTickets = () => {
   }, []);
 
   return (
-    <div className="w-full flex flex-col">
-      <div className="w-full flex justify-end">
-        <button className="border borger-gray-500 rounded px-4 py-2 text-gray-500 transition ease-in-out delay-50 hover:border-purple-500 hover:text-purple-500">
-          ADD TICKET
-        </button>
+    <div className="w-full flex flex-col gap-4">
+      <div className="w-full flex justify-end pr-6">
+        <NavLink to="post-ticket">
+          <button className="border borger-gray-500 rounded px-4 py-2 text-gray-500 transition ease-in-out delay-50 hover:border-purple-500 hover:text-purple-500">
+            ADD TICKET
+          </button>
+        </NavLink>
       </div>
       {error ? (
         <Error errorInfo={error} />
       ) : (
-        <p>{JSON.stringify(fetchedData)}</p>
+        <div className="flex flex-col w-full px-4 gap-8 h-full overflow-y-scroll">
+          {fetchedData.map((data) => (
+            <Ticket key={data.ticket_id} data={data} />
+          ))}
+        </div>
       )}
     </div>
   );
